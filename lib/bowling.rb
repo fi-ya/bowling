ab17d030aa5bf8fb373b26642496e636280a76ff
fi-ya <safiaali@hotmail.com>
2022-03-24 16:19:53 +0000

    test: initialize a frame

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,6 +1,14 @@
 class Bowling 
+
+  attr_reader :frame
+  
+  def initialize 
+    @frame = []
+  end
+
   def throw
     0
   end
+
 
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,11 +1,15 @@
 require 'bowling'
 
 RSpec.describe Bowling do 
+    let(:bowling) {Bowling.new}
   describe '#' do 
     it 'should return zero if it is a gutter throw' do 
       expect(bowling.throw).to eq(0)
     end
     
-
-  end
+    describe '#frame' do
+      it 'shoud return an empty array when a player has not thrown' do
+        expect(bowling.frame).to eq([])
+      end 
+    end
 end
